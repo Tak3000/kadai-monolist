acab192e4fa186f96434be888f614480449f1c1e
Tak <programming.training2018@gmail.com>
2018-09-21 08:04:25 +0000

    kadai-monolist revised

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,13 +14,6 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
-    end  
-#have function追加      
-    if params[:type] == 'have'
-      current_user.have(@item)
-      flash[:success] = '商品を Have しました。'
-#have function追加
-
     end
 
     redirect_back(fallback_location: root_path)
@@ -34,13 +27,46 @@ class OwnershipsController < ApplicationController
       flash[:success] = '商品の Want を解除しました。'
     end
 
-#unhave function追加
+    redirect_back(fallback_location: root_path)
+  end
+  
+  
+
+#have function added
+  def create
+    @item = Item.find_or_initialize_by(code: params[:item_code])
+
+    unless @item.persisted?
+      # @item が保存されていない場合、先に @item を保存する
+      results = RakutenWebService::Ichiba::Item.search(itemCode: @item.code)
+
+      @item = Item.new(read(results.first))
+      @item.save
+    end
+
+    # Want 関係として保存
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+
     if params[:type] == 'Have'
       current_user.unhave(@item) 
       flash[:success] = '商品の Have を解除しました。'
     end
-#unhave function追加
 
     redirect_back(fallback_location: root_path)
   end
+
+#have function added
+
+
+
+
 end
